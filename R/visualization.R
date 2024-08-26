6e9ece1d6d485700ebeaf8ea0e4119cff5fc49da
Estabrook <tgestab@umich.edu>
2024-08-26 12:40:49 -0700

    Added option to disable hillshade in render_terrain

--- R/visualization.R
+++ R/visualization.R
@@ -1,6 +1,6 @@
 topo_lookup <- data.frame(
-  from = c(-999999, seq(-2700, 5400, 300)),
-  to = c(seq(-2701, 5401, 300), 9999),
+  from = c(-999999, seq(-2250, 4500, 250)),
+  to = c(seq(-2251, 4501, 250), 9999),
   color = c('#71ABD8', '#79B2DE', '#84B9E3', '#8DC1EA', '#96C9F0', '#A1D2F7', '#ACDBFB', '#B9E3FF', '#C6ECFF', '#D8F2FE',
             '#ACD0A5', '#94BF8B', '#A8C68F', '#BDCC96', '#D1D7AB', '#E1E4B5', '#EFEBC0', '#E8E1B6',
             '#DED6A3', '#D3CA9D', '#CAB982', '#C3A76B', '#B9985A', '#AA8753', '#AC9A7C', '#BAAE9A',
@@ -8,10 +8,13 @@ topo_lookup <- data.frame(
 )
 
 #' @export
-render_terrain <- function(r){
-  hillshade = shade(terrain(r, v='slope', unit='radians'), terrain(r, v='aspect', unit='radians')
+render_terrain <- function(r, shaded=T){
   terra::plot(r, col=topo_lookup)
-  terra::plot(hillshade, col=grey(0:100/100), range=c(0,1), alpha=0.5 add=T, legend=F)
+
+  if(shaded=T){
+    hillshade = terra::shade(terra::terrain(r, v='slope', unit='radians'), terra::terrain(r, v='aspect', unit='radians'))
+    terra::plot(hillshade, col=grey(0:100/100), range=c(0,1), alpha=0.5, add=T, legend=F)
+  }
 }
 
 
