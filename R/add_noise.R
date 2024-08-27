b8e7bf2049938abe8f962a1b6bf123b909d4612a
Estabrook <tgestab@umich.edu>
2024-08-27 11:12:51 -0700

    Combined absolute and percent noise into one function

--- R/add_noise.R
+++ R/add_noise.R
@@ -1,23 +1,25 @@
 #' @export
-add_percent_noise <- function(r, pct){
-  # get range
-  range <- ceiling((max(terra::values(r, na.rm=T))-min(terra::values(r, na.rm=T)))*(pct/100))
+add_noise <- function(r, type='absolute', amount=terra::res(r)[1]/2){
 
-  noise_rast <- terra::rast(r, vals=0)
-  sample_count <- length(terra::values(noise_rast))
-  terra::values(noise_rast) <- sample(1:range, sample_count, replace=T)
+  if(type=='absolute'){
+    noise_rast <- terra::rast(r, vals=0)
+    sample_count <- length(terra::values(noise_rast))
+    terra::values(noise_rast) <- sample(-1*ceiling(amount):ceiling(amount), sample_count, replace=T)
 
-  return(r+noise_rast)
+    return(r+noise_rast)
+  } else if (type=='percent'){
+    range <- ceiling((max(terra::values(r, na.rm=T))-min(terra::values(r, na.rm=T)))*(amount/100))
+
+    noise_rast <- terra::rast(r, vals=0)
+    sample_count <- length(terra::values(noise_rast))
+    terra::values(noise_rast) <- sample(-1*range:range, sample_count, replace=T)
+
+    return(r+noise_rast)
+  } else {
+    stop("'type' must be 'absolute' or 'percent'.")
+  }
 }
 
-#' @export
-add_absolute_noise <- function(r, noise){
-  noise_rast <- terra::rast(r, vals=0)
-  sample_count <- length(terra::values(noise_rast))
-  terra::values(noise_rast) <- sample(1:ceiling(noise), sample_count, replace=T)
-
-  return(r+noise_rast)
-}
 
 
 
