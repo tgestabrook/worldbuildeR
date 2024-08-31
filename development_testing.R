04e24a7e8eef3f21343c3c01ff498b8bedceca17
Estabrook <tgestab@umich.edu>
2024-08-30 17:06:04 -0700

    More testing

--- development_testing.R
+++ development_testing.R
@@ -1,6 +1,7 @@
 library(devtools)
 devtools::check()
 devtools::load_all()
+library(flowdem)
 
 sample_dem <- terra::rast(file.path('F:', 'LANDIS_Input_Data_Prep', 'Data', 'DEM_90m_OkaMet.tif'))
 sample_dem <- terra::rast(file.path('F:', 'LANDIS_Input_Data_Prep', 'Data', 'DEM_90m_WenEnt.tif'))
@@ -8,35 +9,26 @@ sample_dem <- terra::rast(file.path('F:', 'LANDIS_Input_Data_Prep', 'Data', 'DEM
 terra::plot(sample_dem)
 render_terrain(sample_dem)
 
-terra::plot(worldbuildeR::add_noise(sample_dem))
-
-terra::plot(generate_perlin_noise(sample_dem, frequency=0.005))
-
-terra::plot(generate_fractal_noise(sample_dem, fractal = 'rigid-multi', lacunarity = 1.9, octaves=7, gain=2))
-
-test <- generate_perlin_noise(sample_dem, frequency=0.005)
-terra::plot(test)
-test <- rescale_heightmap(test)
-#terra::plot(test)
-render_terrain(test)
-render_terrain(add_noise(test))
-
-test2 <- generate_fractal_noise(sample_dem, fractal = 'rigid-multi', frequency=0.001, lacunarity = 1.9, octaves=7, gain=2)
+test2 <- generate_fractal_noise(sample_dem, fractal = 'rigid-multi', frequency=0.004, lacunarity = 1.9, octaves=7, gain=2)
 #terra::plot(test2, col=topo_lookup)
 test2 <- rescale_heightmap(test2, 5000, -5000)
-terra::plot(test2)
+test2 <- add_noise(test2)
+test2 <- fill(test2)
+test2 <- add_noise(test2)
+test2 <- fill(test2)
+#terra::plot(test2)
 render_terrain(test2, flat_water = T)
-render_terrain(pinch(test2, w=5), flat_water = T)
+test2 <- incise_flow(test2)
+test2 <- incise_flow(test2)
+test2 <- incise_flow(test2)
+test2 <- incise_flow(test2)
+test2 <- incise_flow(test2)
+render_terrain(test2, flat_water = T)
+test2 <- rainfall_erode(test2, frac = 0.5, precipitons = 5000)
 
-test3 <- thermal_erode(test, iterations = 20)
-render_terrain(test3)
+render_terrain(test2)
 
-test4 <- rainfall_erode(test)
-render_terrain(test4)
-
-test5 <- rainfall_erode(add_noise(test2), precipitons=5000)
-test5 <- rainfall_erode(test5, frac = 0.5, precipitons = 5000)
-render_terrain(test5)
+#test2 <- rainfall_erode(add_noise(test2), precipitons=5000)
 
 start <- Sys.time()
 #.flow_downhill(r, terra::focalValues(.cellrast(r)), 222222)
@@ -48,3 +40,16 @@ start <- Sys.time()
 print(Sys.time()-start)
 
 
+test <- generate_perlin_noise(sample_dem)
+test <- rescale_heightmap(test, 5000,-5000)
+render_terrain(test)
+test <- add_noise(test, amount=30)
+test <- fill(test)
+test <- incise_flow(test)
+render_terrain(test)
+test <- pinch(test)
+test <- rainfall_erode(test)
+
+test4 <- incise_flow(sample_dem)
+test4 <- incise_flow(test4)
+render_terrain(test4)
