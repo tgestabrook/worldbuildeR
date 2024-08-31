a9075fdaa8e3d9b5af054693b5f1f1298480bd5a
Estabrook <tgestab@umich.edu>
2024-08-30 17:04:48 -0700

    Fixed bug due to missing parentheses

--- R/add_noise.R
+++ R/add_noise.R
@@ -4,7 +4,7 @@ add_noise <- function(r, type='absolute', amount=terra::res(r)[1]/2){
   if(type=='absolute'){
     noise_rast <- terra::rast(r, vals=0)
     sample_count <- length(terra::values(noise_rast))
-    terra::values(noise_rast) <- sample(-1*ceiling(amount):ceiling(amount), sample_count, replace=T)
+    terra::values(noise_rast) <- sample((-1*ceiling(amount)):ceiling(amount), sample_count, replace=T)
 
     return(r+noise_rast)
   } else if (type=='percent'){
@@ -12,7 +12,7 @@ add_noise <- function(r, type='absolute', amount=terra::res(r)[1]/2){
 
     noise_rast <- terra::rast(r, vals=0)
     sample_count <- length(terra::values(noise_rast))
-    terra::values(noise_rast) <- sample(-1*range:range, sample_count, replace=T)
+    terra::values(noise_rast) <- sample((-1*range:range), sample_count, replace=T)
 
     return(r+noise_rast)
   } else {
